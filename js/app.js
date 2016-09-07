57891231c0e2ab3816a3e17f9968ea1ad994feca
Tom VanAntwerp <vanantwerp@gmail.com>
2016-09-07 10:00:34 -0400

    Add new data, color scale

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-var width = 1600,
-height = 800;
+var data = 'data/taxes.csv';
+var lowValue = 0;
+var highValue = 55;
+var lowColor = '#ff0000';
+var highColor = '#00ff00';
+
+var width = 1600;
+var height = 800;
+
+var addFill = d3.scale.linear().domain([lowValue, highValue])
+  .interpolate(d3.interpolateHcl)
+  .range([d3.rgb(lowcolor), d3.rgb(highColor)]);
 
 var svg = d3.select('#container').append('svg')
   .attr('width', width).attr('height', height);
@@ -13,7 +23,7 @@ var path = d3.geo.path()
 
 queue()
     .defer(d3.json, 'data/world-50m.json')
-    .defer(d3.csv, 'data/data.csv')
+    .defer(d3.csv, data)
     .await(ready);
 
 function ready(error, world, data) {
@@ -25,21 +35,21 @@ function ready(error, world, data) {
     .append('path')
     .attr('d', path)
     .attr('fill', '#eeeeee')
-    .attr('id', function(d) { return 'c' + d.id; });
+    .attr('id', function (d) { return 'c' + d.id; });
 
-  data.forEach(function(e, i, a) {
+  data.forEach(function (e, i, a) {
     d3.selectAll('#c' + e.id)
-      .attr('fill', addFill(e.tax));
+      .attr('fill', color(e.tax));
   });
 }
 
-function addFill(value) {
-  switch(value) {
-    case 'worldwide':
-      return '#F44336';
-    case 'territorial':
-      return '#4CAF50';
-    default:
-      return '#eeeeee';
-  }
-}
+// function addFill(value) {
+//   switch (value) {
+//     case 'worldwide':
+//       return '#F44336';
+//     case 'territorial':
+//       return '#4CAF50';
+//     default:
+//       return '#eeeeee';
+//   }
+// }
