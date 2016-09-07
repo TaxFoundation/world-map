446ef0cad76f2c877aad02cd7543da91f7f2ca55
Tom VanAntwerp <vanantwerp@gmail.com>
2016-09-07 10:27:53 -0400

    Missing data omitted

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var data = 'data/taxes.csv';
 var lowValue = 0;
 var highValue = 55;
-var lowColor = '#ff0000';
-var highColor = '#00ff00';
+var lowColor = '#91e591';
+var highColor = '#1d085e';
+var year = 2004;
 
-var width = 1600;
-var height = 800;
+var width = 800;
+var height = 400;
 
 var color = d3.scale.linear().domain([lowValue, highValue])
   .interpolate(d3.interpolateHcl)
@@ -14,8 +15,8 @@ var color = d3.scale.linear().domain([lowValue, highValue])
 var svg = d3.select('#container').append('svg')
   .attr('width', width).attr('height', height);
 
-var projection = d3.geo.mercator()
-  .scale(width * 0.1)
+var projection = d3.geo.equirectangular()
+  .scale(width * .15)
   .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -37,7 +38,12 @@ function ready(error, world, data) {
     .attr('fill', '#eeeeee')
     .attr('id', function (d) { return 'c' + d.id; });
 
-  data.forEach(function (e, i, a) {
+  data.filter(function (d) {
+    return d.Year == year;
+  }).filter(function (d) {
+    return d.Rate != '';
+  })
+  .forEach(function (e, i, a) {
     d3.selectAll('#c' + e['UN Country Code'])
       .attr('fill', color(e.Rate));
   });
